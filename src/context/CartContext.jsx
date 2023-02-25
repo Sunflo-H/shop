c2b5f083bde44c02d6c28267030d13c2032d6be9
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-02-25 18:15:46 +0900

    Update: "addCartList 수정"
    carList에 quantity 속성이 추가되어 코드를 수정한다
    상품을 장바구니에 추가 할 때
    이미 있는 상품인지 체크하여
    새 상품으로 등록하거나,
    quantity를 증가시킨다

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
@@ -6,17 +6,29 @@ export const CartProvider = ({ children }) => {
   const [cartList, setCartList] = useState([]);
 
   const addCartList = (product) => {
-    setCartList([...cartList, { product, quantity: 1 }]);
+    // 겹치는 상품이 있을때
+    if (cartList.find((item) => item.product.id === product.id)) {
+      setCartList(
+        cartList.map((item) =>
+          item.product.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    } else setCartList([...cartList, { product, quantity: 1 }]);
   };
 
   const updateCartList = () => {
     setCartList();
   };
 
-  const removeCartList = () => {
-    setCartList();
+  const removeCartList = (product) => {
+    setCartList([...cartList]);
   };
 
+  useEffect(() => {
+    console.log(cartList);
+  }, [cartList]);
   return (
     <CartContext.Provider value={{ cartList, addCartList }}>
       {children}
