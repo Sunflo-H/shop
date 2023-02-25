a48b9b05be4a3ea500189459c09f4fba07789e52
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-02-25 18:36:24 +0900

    Feat: "CartItem에 quantity 적용"
    quantity를 적용해서 개수를 보이게 하고,
    증감 버튼을 눌러 개수를 변화시킨다.
    CartContext에 update, remove 기능 생성

--- src/components/main/cart/CartItem.jsx
+++ src/components/main/cart/CartItem.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { AiOutlinePlusSquare, AiOutlineMinusSquare } from "react-icons/ai";
 import { BsFillTrashFill } from "react-icons/bs";
+import { CartContext } from "../../../context/CartContext";
 
 export default function CartItem({ product }) {
   const { id, name, imageUrl, price, description, size, category } = product;
+  const { cartList, updateCartList, removeCartList } = useContext(CartContext);
+
+  // const [quantity, setQuantity] = useState(
+  //   cartList.find((item) => item.product === product).quantity
+  // );
+
+  const quantity = cartList.find((item) => item.product === product).quantity;
+
+  const handlePlusBtnClick = () => {
+    updateCartList(product, "plus");
+  };
+
+  const handleMinusBtnClick = () => {
+    updateCartList(product, "minus");
+  };
+
+  const handleRemoveBtnClick = () => {
+    removeCartList(product);
+  };
+
   return (
     <div className="flex my-2">
       <div className="w-40">
@@ -24,16 +45,25 @@ export default function CartItem({ product }) {
       </div>
       <div className="flex my-auto ml-auto">
         <div className="py-1.5 ">
-          <AiOutlineMinusSquare className="text-lg cursor-pointer" />
+          <AiOutlineMinusSquare
+            className="text-lg cursor-pointer"
+            onClick={handleMinusBtnClick}
+          />
         </div>
         <div className="px-3">
-          <span className="text-xl">1</span>
+          <span className="text-xl">{quantity}</span>
         </div>
         <div className="py-1.5">
-          <AiOutlinePlusSquare className="text-lg cursor-pointer" />
+          <AiOutlinePlusSquare
+            className="text-lg cursor-pointer"
+            onClick={handlePlusBtnClick}
+          />
         </div>
         <div className="py-1.5 pl-6">
-          <BsFillTrashFill className="text-lg cursor-pointer" />
+          <BsFillTrashFill
+            className="text-lg cursor-pointer"
+            onClick={handleRemoveBtnClick}
+          />
         </div>
       </div>
     </div>
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -7,10 +7,10 @@ export const CartProvider = ({ children }) => {
 
   const addCartList = (product) => {
     // 겹치는 상품이 있을때
-    if (cartList.find((item) => item.product.id === product.id)) {
+    if (cartList.find((item) => item.product === product)) {
       setCartList(
         cartList.map((item) =>
-          item.product.id === product.id
+          item.product === product
             ? { ...item, quantity: item.quantity + 1 }
             : item
         )
@@ -18,19 +18,38 @@ export const CartProvider = ({ children }) => {
     } else setCartList([...cartList, { product, quantity: 1 }]);
   };
 
-  const updateCartList = () => {
-    setCartList();
+  const updateCartList = (product, type) => {
+    console.log(product, type);
+    if (type === "plus")
+      setCartList(
+        cartList.map((item) =>
+          item.product === product
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    else
+      setCartList(
+        cartList.map((item) =>
+          item.product === product && item.quantity !== 1
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+      );
   };
 
   const removeCartList = (product) => {
-    setCartList([...cartList]);
+    setCartList(cartList.filter((item) => item.product !== product));
   };
 
   useEffect(() => {
     console.log(cartList);
   }, [cartList]);
+
   return (
-    <CartContext.Provider value={{ cartList, addCartList }}>
+    <CartContext.Provider
+      value={{ cartList, addCartList, updateCartList, removeCartList }}
+    >
       {children}
     </CartContext.Provider>
   );
