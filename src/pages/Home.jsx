b3797f053cb103ee271b47501639349b079a319b
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-03-23 13:02:43 +0900

    Feat: #4 - "컴포넌트화한 컨텐츠적용"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,7 @@ import "react-awesome-slider/dist/custom-animations/cube-animation.css";
 import "react-awesome-slider/dist/styles.css";
 import Carousel from "../components/carousel/Carousel";
 import ProductsByCategory from "../components/main/product/ProductsByCategory";
+import HomeContents_blog from "../components/main/Home/HomeContents_blog";
 
 export default function Home() {
   const [category, setCategory] = useState("Men");
@@ -121,119 +122,7 @@ export default function Home() {
         </section>
 
         <div className="w-full h-full bg-gray-100 px-32 pt-14 py-10 mb-20 ">
-          <h1 className="text-5xl font-bold mb-8">Our Stories</h1>
-          <div className="flex gap-10 mb-32">
-            <div className="basis-1/4 cursor-pointer">
-              <img
-                className="mb-4"
-                src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_405,w_270/v1679413296/shoppy/blog4_gmrzw8.jpg"
-                alt=""
-              />
-              <h1 className="text-xl mb-4">
-                Creative Spirits Series: Nails by Mei X J.Crew
-              </h1>
-              <span className="font-bold border-b-2 border-black pb-1">
-                Shop the jewelry collab
-              </span>
-            </div>
-            <div className="basis-1/4 cursor-pointer">
-              <img
-                className="mb-4"
-                src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_405,w_270/v1679413299/shoppy/blog1_uot0ej.jpg"
-                alt=""
-              />
-              <h1 className="text-xl mb-4">The edit: embellished everything</h1>
-              <span className="font-bold border-b-2 border-black pb-1">
-                Shop the roundup
-              </span>
-            </div>
-            <div className="basis-1/4 cursor-pointer">
-              <img
-                className="mb-4"
-                src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_405,w_270/v1679413298/shoppy/blog3_ufgcyf.jpg"
-                alt=""
-              />
-              <h1 className="text-xl mb-4">Olympia’s picks: March edition</h1>
-              <span className="font-bold border-b-2 border-black pb-1">
-                Shop her favorite styles
-              </span>
-            </div>
-            <div className="basis-1/4 cursor-pointer">
-              <img
-                className="mb-4"
-                src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_405,w_270/v1679413299/shoppy/blog2_twfun4.jpg"
-                alt=""
-              />
-              <h1 className="text-xl mb-4">Local time with Leigh Altshuler</h1>
-              <span className="font-bold border-b-2 border-black pb-1">
-                Explore her NYC bookstore
-              </span>
-            </div>
-          </div>
-          <div className="flex border-t-4 border-gray-900 ">
-            <div className="flex flex-wrap w-3/4  items-start ">
-              <div className="basis-1/3 pr-8 pb-6 h-80 ">
-                <img
-                  className="w-full h-full"
-                  src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_296,w_270/v1679413298/shoppy/blog8_ewqvt6.jpg"
-                  alt=""
-                />
-              </div>
-              <div className="basis-1/3 pr-8 pb-6 h-80 ">
-                <img
-                  className="w-full h-full"
-                  src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_296,w_270/v1679413297/shoppy/blog6_g5hgqh.jpg"
-                  alt=""
-                />
-              </div>
-              <div className="basis-1/3 pr-8 pb-6 h-80 ">
-                <img
-                  className="w-full h-full"
-                  src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_296,w_270/v1679413299/shoppy/blog7_znvyes.jpg"
-                  alt=""
-                />
-              </div>
-              <div className="basis-1/3 pr-8 pt-2 pb-4 h-80">
-                <img
-                  className="w-full h-full"
-                  src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_296,w_270/v1679413297/shoppy/blog5_ynq6j0.jpg"
-                  alt=""
-                />
-              </div>
-              <div className="basis-1/3 pr-8 pt-2 pb-4 h-80">
-                <img
-                  className="w-full h-full"
-                  src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_296,w_270/v1679413298/shoppy/blog9_vmczy4.jpg"
-                  alt=""
-                />
-              </div>
-              <div className="basis-1/3 pr-8 pt-2 pb-4 h-80">
-                <img
-                  className="w-full h-full"
-                  src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_296,w_270/v1679413298/shoppy/blog10_cw41yu.jpg"
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="flex flex-col justify-center items-center w-1/4 ">
-              <div className="relative">
-                <div className="absolute -top-5 -left-10 mr-auto ml-6 font-bold">
-                  MEETS CREATIVITY
-                </div>
-                <span className="absolute top-11 -left-16 font-bold -rotate-90  ">
-                  WHERE STYLE
-                </span>
-                <div className="flex items-end w-52 h-52 border-4 border-black mb-10 p-4 text-2xl font-bold">
-                  <h1>
-                    The <br />
-                    Adonis <br /> Collective
-                  </h1>
-                </div>
-              </div>
-
-              <Button text="Meet our Community" />
-            </div>
-          </div>
+          <HomeContents_blog />
         </div>
       </main>
       <footer className="border-gray-400 border-t">
