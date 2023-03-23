c9f3d15fd35dc2a91067dab8d10247006c510e8d
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-03-23 15:39:40 +0900

    Feat: #4 - "HomeContents_blog에 BlogBottom 컴포넌트 적용"

--- src/components/main/Home/HomeContents_blog.jsx
+++ src/components/main/Home/HomeContents_blog.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../../ui/Button";
+import BlogBottom from "./BlogBottom";
 import BlogTopImage from "./BlogTopImage";
 
 export default function HomeContents_blog() {
@@ -38,69 +39,12 @@ export default function HomeContents_blog() {
           return <BlogTopImage info={info} key={i} />;
         })}
       </div>
-      <div className="flex border-t-4 border-gray-900 ">
-        <div className="flex flex-wrap w-3/4  items-start ">
-          <div className="basis-1/3 pr-8 pb-6 h-80 ">
-            <img
-              className="w-full h-full"
-              src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_296,w_270/v1679413298/shoppy/blog8_ewqvt6.jpg"
-              alt=""
-            />
-          </div>
-          <div className="basis-1/3 pr-8 pb-6 h-80 ">
-            <img
-              className="w-full h-full"
-              src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_296,w_270/v1679413297/shoppy/blog6_g5hgqh.jpg"
-              alt=""
-            />
-          </div>
-          <div className="basis-1/3 pr-8 pb-6 h-80 ">
-            <img
-              className="w-full h-full"
-              src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_296,w_270/v1679413299/shoppy/blog7_znvyes.jpg"
-              alt=""
-            />
-          </div>
-          <div className="basis-1/3 pr-8 pt-2 pb-4 h-80">
-            <img
-              className="w-full h-full"
-              src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_296,w_270/v1679413297/shoppy/blog5_ynq6j0.jpg"
-              alt=""
-            />
-          </div>
-          <div className="basis-1/3 pr-8 pt-2 pb-4 h-80">
-            <img
-              className="w-full h-full"
-              src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_296,w_270/v1679413298/shoppy/blog9_vmczy4.jpg"
-              alt=""
-            />
-          </div>
-          <div className="basis-1/3 pr-8 pt-2 pb-4 h-80">
-            <img
-              className="w-full h-full"
-              src="https://res.cloudinary.com/dysdtbktd/image/upload/c_scale,h_296,w_270/v1679413298/shoppy/blog10_cw41yu.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className="flex flex-col justify-center items-center w-1/4 ">
-          <div className="relative">
-            <div className="absolute -top-5 -left-10 mr-auto ml-6 font-bold">
-              MEETS CREATIVITY
-            </div>
-            <span className="absolute top-11 -left-16 font-bold -rotate-90  ">
-              WHERE STYLE
-            </span>
-            <div className="flex items-end w-52 h-52 border-4 border-black mb-10 p-4 text-2xl font-bold">
-              <h1>
-                The <br />
-                Adonis <br /> Collective
-              </h1>
-            </div>
-          </div>
-
-          <Button text="Meet our Community" />
-        </div>
+      <div
+        className="flex 
+      flex-col-reverse lg:flex-row lg:border-t-4 lg:border-gray-900 
+      "
+      >
+        <BlogBottom />
       </div>
     </>
   );
