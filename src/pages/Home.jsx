1ec9ae7a7f1b081d3b8b53544fd62a5268068852
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-03-23 16:06:15 +0900

    Feat: #3 - "컴포넌트화"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,11 @@
-import React, { useState } from "react";
-import AwesomeSlider from "react-awesome-slider";
+import React from "react";
 import Footer from "../components/footer/Footer";
-import Banner from "../components/main/Banner";
-import HomeContents from "../components/main/HomeContents";
-import HomeProducts from "../components/main/product/HomeProducts";
 import Button from "../components/ui/Button";
-
-import "react-awesome-slider/dist/custom-animations/cube-animation.css";
-import "react-awesome-slider/dist/styles.css";
 import Carousel from "../components/carousel/Carousel";
-import ProductsByCategory from "../components/main/product/ProductsByCategory";
 import HomeContents_blog from "../components/main/Home/HomeContents_blog";
+import HomeContents_ProductsByCategory from "../components/main/Home/HomeContents_ProductsByCategory";
 
 export default function Home() {
-  const [category, setCategory] = useState("Men");
   return (
     <div>
       <main className="px-5 md:px-10 m-auto max-w-screen-2xl ">
@@ -45,6 +37,7 @@ export default function Home() {
           </div>
         </section>
 
+        {/* Accessories Contents */}
         <section className="relative w-full h-screen max-h-96 mb-20 bg-black">
           <div className="w-full h-full bg-cover bg-center bg-accessories opacity-70"></div>
           <div className="absolute w-full top-28 text-center ">
@@ -57,70 +50,12 @@ export default function Home() {
           </div>
         </section>
 
+        {/* Product by category Contents */}
         <section className="w-full flex flex-col m-auto mb-20 ">
-          <div className="text-center mb-12">
-            <p className="text-md mb-4 tracking-widest">
-              <span className="font-bold mr-4">NEW</span>
-              <span className="">COLLECTIONS</span>
-            </p>
-            <h2 className="text-5xl font-semibold">Featured Items</h2>
-          </div>
-          <ul className="flex justify-center gap-8 mb-10">
-            <li
-              className={`cursor-pointer hover:border-black border-b-2 
-              ${
-                category === "Men"
-                  ? "border-black font-bold"
-                  : "border-transparent"
-              }`}
-              onClick={() => setCategory("Men")}
-            >
-              Men
-            </li>
-            <li
-              className={`cursor-pointer hover:border-black border-b-2 "
-              ${
-                category === "Women"
-                  ? "border-black font-bold"
-                  : "border-transparent"
-              }`}
-              onClick={() => setCategory("Women")}
-            >
-              Women
-            </li>
-            <li
-              className={`cursor-pointer hover:border-black border-b-2 
-              ${
-                category === "Accessories"
-                  ? "border-black font-bold"
-                  : "border-transparent"
-              }`}
-              onClick={() => setCategory("Accessories")}
-            >
-              Accessories
-            </li>
-            <li
-              className={`cursor-pointer hover:border-black border-b-2 
-              ${
-                category === "Shoes"
-                  ? "border-black font-bold"
-                  : "border-transparent"
-              }`}
-              onClick={() => setCategory("Shoes")}
-            >
-              Shoes
-            </li>
-          </ul>
-          <div className="flex justify-center  mb-14">
-            <div className="md:w-2/3">
-              <HomeProducts category={category} />
-            </div>
-          </div>
-          <div className="text-center ">
-            <Button text="See All" />
-          </div>
+          <HomeContents_ProductsByCategory />
         </section>
 
+        {/* Blog Contents */}
         <div className="w-full h-full bg-gray-100 px-4 md:px-32 pt-14 py-10 mb-20 ">
           <HomeContents_blog />
         </div>
