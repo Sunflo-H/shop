669f49fc3f871f3764fd1d11106c48c0584be5b1
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-04-03 23:31:05 +0900

    Feat: "찜 상태가 업데이트되는 함수에 useMutation 코드를 추가" #10
    - 업데이트된 데이터가 바로 적용되게 하기 위함

--- src/hooks/useFavorites.jsx
+++ src/hooks/useFavorites.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { addFavorites, getFavorites, removeFavorites } from "../api/firebase";
 import { useAuthContext } from "../context/AuthContext";
 import { useState } from "react";
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { useMemo } from "react";
 import { useEffect } from "react";
 
-export default function useFavorites(product) {
+export default function useFavorites(product, currentCategory) {
   const { user, uid } = useAuthContext();
+  const queryClient = useQueryClient();
   const [isFavorite, setIsFavorite] = useState(false); // 찜
   const favoriteQuery = useQuery(
     ["favorites", uid],
@@ -21,9 +22,13 @@ export default function useFavorites(product) {
 
   useEffect(() => {
     setIsFavorite(favoritesIdSet.has(product.id));
-  }, [favoriteQuery.data]);
+  }, [favoriteQuery.data, currentCategory]);
 
-  const handleFavoriteClick = () => {
+  /**
+   * db의 favorites data 와 isFavorite State를 조작하는 함수
+   * 클릭이벤트에 사용하여 찜 상태를 db에 전달한다.
+   */
+  const favoriteClick = () => {
     if (user) {
       setIsFavorite((prev) => {
         !prev ? addFavorites(product, uid) : removeFavorites(product, uid);
@@ -34,5 +39,11 @@ export default function useFavorites(product) {
     }
   };
 
-  return { isFavorite, handleFavoriteClick };
+  const updateFavorites = useMutation(favoriteClick, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["favorites", uid]);
+    },
+  });
+
+  return { isFavorite, updateFavorites };
 }
