504520445792d06d93daf0d0b37df2d1435de10b
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-04-03 09:36:36 +0900

    Feat: "해당 상품의 디테일 페이지로 이동 및 AI loading 표현" #14
    - 이미지 추천이 실행되면 버튼을 클릭하여 해당 상품의 디테일 페이지로 이동한다.
    - ai 작동중일때 로딩 메세지 추가

--- src/pages/Recommend.jsx
+++ src/pages/Recommend.jsx
@@ -5,13 +5,15 @@ import { imageUrlQueries } from "../api/firebase";
 import useProducts from "../hooks/useProducts";
 import { useEffect } from "react";
 import Button from "../components/ui/Button";
+import { Link } from "react-router-dom";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function Recommend() {
   const [inputText, setInputText] = useState("");
   const [recommendation, setRecommendation] = useState("");
   const recommendationText = recommendation.split("@")[1];
   const recommendationImage = recommendation.split("@")[0];
-  console.log(recommendation);
+  const [isAiLoading, setIsAiLoading] = useState(false);
 
   const genders = ["Men", "Women"];
   const [gender, setGender] = useState("Men");
@@ -22,18 +24,24 @@ export default function Recommend() {
   let menUrls = men.data?.map((product) => product.imageUrl);
   let womenUrls = women.data?.map((product) => product.imageUrl);
 
-  // let product;
-  // if (gender === "Men") {
-  //   product = productsQueries[0].data.find((data) => {
-  //     // console.log(data.imageUrl === recommendationImage.replace(" ", ""));
-  //     if (data.imageUrl === recommendationImage.replace(" ", "")) return data;
-  //   });
-  // } else {
-  //   product = productsQueries[1].data.find((data) => {
-  //     // console.log(data.imageUrl === recommendationImage.replace(" ", ""));
-  //     if (data.imageUrl === recommendationImage.replace(" ", "")) return data;
-  //   });
-  // }
+  let product = recommendation && getProductData(recommendationImage);
+
+  useEffect(() => {
+    setIsAiLoading(false);
+    console.log(recommendation);
+  }, [recommendation]);
+
+  function getProductData(url) {
+    if (gender === "Men") {
+      return productsQueries[0].data.find(
+        (data) => data.imageUrl === recommendationImage.replace(" ", "")
+      );
+    } else {
+      return productsQueries[1].data.find(
+        (data) => data.imageUrl === recommendationImage.replace(" ", "")
+      );
+    }
+  }
 
   const handleInputChange = (event) => {
     setInputText(event.target.value);
@@ -41,28 +49,31 @@ export default function Recommend() {
 
   const getClothingRecommendation = async () => {
     try {
+      setIsAiLoading(true);
       const question = `${gender}'s ${inputText}`;
 
       const prompt = `
-        나는 옷 스타일을 추천하는 전문가 AI입니다.
-        나는 색을 위주로 추천합니다.  
-        배경과 모델을 정확하게 구분합니다.
-        배경은 추천 기준에 포함하지 않습니다.
-        이미지를 매우 정확하게 인식합니다.
-        나는 항상 추천 이유와 url을 알려줍니다.
-        항상 url을 먼저 알려주고, 이유를 말합니다.
-        url 답변의 형태는 "http://경로.jpg@" 입니다.
-        추천 이유 외의 다른 말은 하지 않습니다.
+      I am an expert AI who recommends clothes style.
+      I mainly recommend colors.
+      Accurately distinguish between background and model.
+      The background is not included in the recommendation criteria.
+      Recognize images very accurately.
+      I always let you know the reason for recommendation and url.
+      Always tell me url first, and tell me why.
+
+        url 답변의 형태는 항상 "http://경로.jpg@" 입니다. @를 무조건 사용합니다.
+        The reason for the recommendation is up to two lines.
+        I don't say anything other than the reason for recommendation.
+
         주어진 배열의 이미지를 모두 찾아보고 가장 유사한 이미지 하나를 추천해주고, 이유를 설명합니다. 꼭 모두 찾아보고 비교합니다.: ${
           gender === "Men"
             ? shuffle(menUrls).join(", ")
             : shuffle(womenUrls).join(", ")
         }.
 
-        만약 이미지의 인식이 정확하게 되지 않는다면 해당 이미지를 포기하고, 대신 다른 이미지를 인식합니다.
-      
+        If the image is not recognized correctly, give up the image, and instead recognize another image.
         한번 말한 url은 모든 절반 이상의 url을 말하기 전까지, 다시는 말하지 않습니다.
-        영어로 대답합니다.
+        
  
        ${question} The answer is:`;
       const response = await axios.post(
@@ -77,7 +88,7 @@ export default function Recommend() {
         {
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer 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`,
+            Authorization: `Bearer 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`,
           },
         }
       );
@@ -98,23 +109,28 @@ export default function Recommend() {
             recommendationImage ? "flex flex-col" : "hidden"
           }`}
         >
-          <div className="h-3/4 mb-4">
+          <div className="h-3/4 ">
             <img className={`w-full h-full`} src={recommendationImage}></img>
           </div>
           <div
-            className={`${recommendationImage ? "block font-bold" : "hidden"}`}
+            className={`${
+              recommendationImage ? "block font-bold py-3" : "hidden"
+            }`}
           >
             {recommendationText?.replace(".", "")}
           </div>
-          <div
+
+          {/* 온 클릭 */}
+          <Link
+            to={`${product && `/products/${product.category}/${product.id}`}`}
+            state={{ product }}
             className="w-40 bg-black text-white text-center m-auto py-2 cursor-pointer"
-            onCLick={"asd"}
           >
             See now
-          </div>
+          </Link>
         </div>
-        <div className="flex flex-col justify-center w-2/5 h-full">
-          <div className="flex flex-col py-4 border border-gray-300 rounded-lg">
+        <div className=" flex flex-col justify-center w-2/5 h-full">
+          <div className="relative flex flex-col py-4 border border-gray-300 rounded-lg">
             <Option_gender
               genders={genders}
               currentGender={gender}
@@ -122,7 +138,7 @@ export default function Recommend() {
             />
 
             <textarea
-              className="px-10 py-4 max-h-96"
+              className="px-10 py-4 max-h-80"
               value={inputText}
               onChange={handleInputChange}
               placeholder="Enter a description for your preferred clothing style..."
@@ -135,6 +151,14 @@ export default function Recommend() {
               onClick={getClothingRecommendation}
             >
               Get Recommendation
+            </div>
+            <div
+              className={`absolute -bottom-10 flex justify-center items-center gap-4 w-full ${
+                isAiLoading ? "block" : "hidden"
+              }`}
+            >
+              Ai is looking for styles to recommend.{" "}
+              <AiOutlineLoading3Quarters className="animate-spin" />
             </div>
           </div>
         </div>
