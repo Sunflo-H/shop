e56c24007ed65b4e40eecefa653609561d360200
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-04-03 18:16:33 +0900

    Feat: "favorites 기능 추가" #10
    - 하트 아이콘을 클릭하면 DB에 정보가 저장된다.
    - 하트 아이콘의 클릭하면 색이 바뀐다.

--- src/components/main/product/ProductCard.jsx
+++ src/components/main/product/ProductCard.jsx
@@ -1,14 +1,29 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AiFillHeart } from "react-icons/ai";
+import {
+  addFavorites,
+  getFavorites,
+  removeFavorites,
+} from "../../../api/firebase";
+import { useAuthContext } from "../../../context/AuthContext";
+import { useQuery } from "react-query";
 
-export default function ProductCard({ product }) {
+export default function ProductCard({ product, favorites }) {
   const { title, imageUrl, category, price, id } = product;
   const [isLove, setIsLove] = useState(false); // 찜
+  const { uid } = useAuthContext();
   const navigate = useNavigate();
 
   const handleProductClick = () => {
     navigate(`/products/${category}/${id}`, { state: { product } });
+  };
+
+  const handleFavoriteClick = () => {
+    setIsLove((prev) => {
+      !prev ? addFavorites(product, uid) : removeFavorites(product, uid);
+      return !prev;
+    });
   };
 
   return (
@@ -17,7 +32,7 @@ export default function ProductCard({ product }) {
         <span className="bg-black text-white py-px px-1 text-sm">New</span>
         <AiFillHeart
           className={`text-2xl cursor-pointer ${isLove && "text-rose-500"}`}
-          onClick={() => setIsLove((prev) => !prev)}
+          onClick={handleFavoriteClick}
         />
       </div>
       <div className="w-full" onClick={handleProductClick}>
