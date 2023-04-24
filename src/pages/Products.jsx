f7ab07376547312db97661989638a26029d9ddd5
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-04-24 10:01:19 +0900

    Feat: "페이지 이동시 스크롤 맨 위로 이동하는 기능 추가" #15

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import ProductsByCategory from "../components/main/product/ProductsByCategory";
+import { useEffect } from "react";
 
 export default function Products() {
   const location = useLocation();
   const category = location.state;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return <ProductsByCategory category={category} />;
 }
