7b8732d6b7a3b6b488f8d2c11907c984d33c1773
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-04-24 09:59:15 +0900

    Feat: "상품이 없는 경우 보여줄 컴포넌트 생성 및 적용 완료" #15

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -5,6 +5,7 @@ import CartItem from "../components/main/cart/CartItem";
 import PriceCard from "../components/main/cart/PriceCard";
 
 import useCart from "../hooks/useCart";
+import EmptyProduct from "../components/error/EmptyProduct";
 
 const DELIVERY_FEE = 3;
 
@@ -28,29 +29,38 @@ export default function MyCart() {
       <div className="text-center py-2 px-4 border-b">
         <span className="text-xl font-bold">My Cart</span>
       </div>
-      <div className="px-5">
-        {productsInCart &&
-          productsInCart.map((product, i) => (
-            <CartItem product={product} key={i} />
-          ))}
+      {productsInCart ? (
+        <EmptyProduct />
+      ) : (
+        <>
+          <div className="px-5">
+            {productsInCart &&
+              productsInCart.map((product, i) => (
+                <CartItem product={product} key={i} />
+              ))}
 
-        <div className="flex justify-around border-t mt-10 py-4 bg-gray-100">
-          <PriceCard title="Product total" price={allPrice()} />
-          <div className="py-7">
-            <AiFillPlusCircle className="text-xl" />
+            <div className="flex justify-around border-t mt-10 py-4 bg-gray-100">
+              <PriceCard title="Product total" price={allPrice()} />
+              <div className="py-7">
+                <AiFillPlusCircle className="text-xl" />
+              </div>
+              <PriceCard title="Shipping cost" price={DELIVERY_FEE} />
+
+              <div className="py-7">
+                <FaEquals className="text-xl" />
+              </div>
+              <PriceCard
+                title="Total price"
+                price={allPrice() + DELIVERY_FEE}
+              />
+            </div>
+
+            <div className="bg-black text-center py-1 mt-10 mb-20 cursor-pointer">
+              <div className="text-white font-bold py-2">주문하기</div>
+            </div>
           </div>
-          <PriceCard title="Shipping cost" price={DELIVERY_FEE} />
-
-          <div className="py-7">
-            <FaEquals className="text-xl" />
-          </div>
-          <PriceCard title="Total price" price={allPrice() + DELIVERY_FEE} />
-        </div>
-
-        <div className="bg-black text-center py-1 mt-10 mb-20 cursor-pointer">
-          <div className="text-white font-bold py-2">주문하기</div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 }
--- src/pages/MyFavorites.jsx
+++ src/pages/MyFavorites.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import useFavorites from "../hooks/useFavorites";
 import ProductCard from "../components/main/product/ProductCard";
+import { useEffect } from "react";
+import EmptyProduct from "../components/error/EmptyProduct";
 
 export default function MyFavorites() {
   const { favoriteQuery } = useFavorites();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className="pt-20 px-4">
       <div className="text-center py-2 px-4 border-b">
-        <span className="text-2xl font-bold">My Favorites</span>
+        <span className="text-xl font-bold">My Favorites</span>
       </div>
-      <div
-        className={`grid grid-cols-2 md:grid-cols-3 mt-8
+      {favoriteQuery ? (
+        <EmptyProduct />
+      ) : (
+        <div
+          className={`grid grid-cols-2 md:grid-cols-3 mt-8
         max-w-screen-2xl m-auto px-10 gap-5`}
-      >
-        {favoriteQuery?.data?.map((product) => (
-          <ProductCard product={product} />
-        ))}
-      </div>
+        >
+          {favoriteQuery?.data?.map((product) => (
+            <ProductCard product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
