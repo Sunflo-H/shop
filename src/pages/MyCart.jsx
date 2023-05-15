d2d51eedbeb5883f61bc4a1c3d6baffc07085ea9
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-05-15 14:39:39 +0900

    Feat: "Cart 페이지 최대 너비 지정하기" #15

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { FaEquals } from "react-icons/fa";
 import CartItem from "../components/main/cart/CartItem";
@@ -24,43 +24,44 @@ export default function MyCart() {
     );
   };
 
+  useEffect(() => {
+    console.log(productsInCart);
+  }, [productsInCart]);
+
   return (
-    <div className="px-4 pt-20">
+    <div className="px-4 pt-20 ">
       <div className="text-center py-2 px-4 border-b">
         <span className="text-xl font-bold">My Cart</span>
       </div>
-      {productsInCart ? (
+      {/* {productsInCart ? (
         <EmptyProduct />
-      ) : (
-        <>
-          <div className="px-5">
-            {productsInCart &&
-              productsInCart.map((product, i) => (
-                <CartItem product={product} key={i} />
-              ))}
+      ) : ( */}
+      <>
+        <div className="px-5 max-w-screen-2xl m-auto">
+          {productsInCart &&
+            productsInCart.map((product, i) => (
+              <CartItem product={product} key={i} />
+            ))}
 
-            <div className="flex justify-around border-t mt-10 py-4 bg-gray-100">
-              <PriceCard title="Product total" price={allPrice()} />
-              <div className="py-7">
-                <AiFillPlusCircle className="text-xl" />
-              </div>
-              <PriceCard title="Shipping cost" price={DELIVERY_FEE} />
-
-              <div className="py-7">
-                <FaEquals className="text-xl" />
-              </div>
-              <PriceCard
-                title="Total price"
-                price={allPrice() + DELIVERY_FEE}
-              />
+          <div className="flex justify-around border-t mt-10 py-4 bg-gray-100">
+            <PriceCard title="Product total" price={allPrice()} />
+            <div className="py-7">
+              <AiFillPlusCircle className="text-xl" />
             </div>
+            <PriceCard title="Shipping cost" price={DELIVERY_FEE} />
 
-            <div className="bg-black text-center py-1 mt-10 mb-20 cursor-pointer">
-              <div className="text-white font-bold py-2">주문하기</div>
+            <div className="py-7">
+              <FaEquals className="text-xl" />
             </div>
+            <PriceCard title="Total price" price={allPrice() + DELIVERY_FEE} />
           </div>
-        </>
-      )}
+
+          <div className="bg-black text-center py-1 mt-10 mb-20 cursor-pointer">
+            <div className="text-white font-bold py-2">주문하기</div>
+          </div>
+        </div>
+      </>
+      {/* )} */}
     </div>
   );
 }
