d9f9e62cb0cc0754f79f7b988690ccc476b4c28a
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-12-05 07:49:14 +0900

    Update : 불필요한 파일삭제 #17
    ProductsByCategory.jsx 삭제
    그 내용을 상위 컴포넌트인 Products에 적용
    
    getTitle 함수 생성

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,14 +1,52 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import ProductsByCategory from "../components/main/product/ProductsByCategory";
 import { useEffect } from "react";
+import useProducts from "../hooks/useProducts";
+import ProductCard from "../components/main/product/ProductCard";
 
 export default function Products() {
-  const location = useLocation();
-  const category = location.state;
+  const { state: category } = useLocation();
+  const {
+    productsQuery: { data },
+  } = useProducts(category);
+  const title = getTitle(category);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [category]);
 
-  return <ProductsByCategory category={category} />;
+  return (
+    <section className="pt-20">
+      {category && (
+        <h1 className="text-center text-2xl lg:text-4xl my-14">{title}</h1>
+      )}
+      <div
+        className={`grid grid-cols-2 
+        md:grid-cols-3
+        max-w-screen-2xl m-auto px-10 gap-5`}
+      >
+        {data &&
+          data.map((product, index) => {
+            return (
+              <ProductCard
+                product={product}
+                key={index}
+                currentCategory={category}
+              />
+            );
+          })}
+      </div>
+    </section>
+  );
+}
+
+/**
+ * category 값에 따라 제목을 반환하는 함수
+ * @returns title
+ */
+function getTitle(category) {
+  return category === "Men" || category === "Women"
+    ? `SHOP ALL ${category.toUpperCase()}'S CLOTHING
+      `
+    : `The ${category} Shop`;
 }
