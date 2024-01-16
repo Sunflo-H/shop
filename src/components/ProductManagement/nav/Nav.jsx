3ff0c22c480629bcc53045c7a6626196a46e9ff5
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-01-16 21:18:16 +0900

    Feat : 상품관리 페이지 nav 작성 #18

--- src/components/ProductManagement/nav/Nav.jsx
+++ src/components/ProductManagement/nav/Nav.jsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiSettings } from "react-icons/fi";
+import { FaCaretDown } from "react-icons/fa";
+import { FaCaretUp } from "react-icons/fa";
 
+const category = ["All", "Men", "Women", "Shoes", "Accessories"];
 export default function Nav() {
+  const [currentCategory, setCurrentCategory] = useState(category[0]);
+
+  const handleCategoryClick = (e) => {
+    console.log(e.target);
+  };
+  return (
+    <div className="w-auto h-full text-lg font-bold bg-white ml-6 pb-4 ">
+      <div className="flex gap-2 text-blue-500 px-8 py-4 ">
+        Management <FiSettings className="self-center" />
+      </div>
+
+      <div className=" py-1">
+        <div className="flex gap-1 px-3 pb-2 cursor-pointer">
+          <FaCaretDown className="self-center" /> Category
+        </div>
+        {category.map((item, index) => (
+          <CategoryItem
+            value={item}
+            currentCategory={currentCategory}
+            onClick={handleCategoryClick}
+            key={index}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function CategoryItem({ value, currentCategory }) {
   const activeCategoryStyle = `text-blue-600 bg-blue-100`;
   return (
-    <div className="w-auto h-full text-lg font-bold bg-white ml-6 ">
-      <div className="flex gap-2 text-blue-500 p-4 ">
-        Category Management <FiSettings className="self-center" />
-      </div>
-      <div className="px-4 py-1 text-blue-600 bg-blue-100">All Category</div>
+    <div
+      className={`pl-12 py-2 cursor-pointer hover:bg-blue-100 ${
+        currentCategory === value ? activeCategoryStyle : ""
+      } `}
+    >
+      {value}
     </div>
   );
 }
