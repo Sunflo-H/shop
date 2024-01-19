291d31d0da7801423b91764a708fe5b683d15cdb
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-01-19 23:29:49 +0900

    Feat : Header에서 버튼에 기능 적용 #18

--- src/components/ProductManagement/header/Header.jsx
+++ src/components/ProductManagement/header/Header.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function header() {
+export default function Header() {
+  const navigate = useNavigate();
+  const handleAddProductClick = () => {
+    navigate("/products/new");
+  };
   return (
     <div className="w-screen flex px-4 py-2 border-b border-gray-300 bg-white mb-8 ">
       <div className="flex self-center top-1/2 text-xl font-bold ml-4">
         Product Management
       </div>
-      <div className="ml-auto bg-blue-500 text-white p-2 cursor-pointer">
+      <div
+        className="ml-auto bg-blue-500 text-white p-2 cursor-pointer"
+        onClick={handleAddProductClick}
+      >
         Add Product
       </div>
     </div>
