65770f5567d3dd8596a8b2e9d3db11b303a2fb30
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-01-25 21:43:42 +0900

    Feat : 상품 등록 - 상품의 status별 수를 가져오는 기능 추가 #18
    상품의 모든 status [ All, Sale, Sold Out, Hide] 에 따라 각 항목 옆에 개수를 나타냅니다.

--- src/components/ProductManagement/main/ProductStatus.jsx
+++ src/components/ProductManagement/main/ProductStatus.jsx
@@ -2,11 +2,7 @@ import React, { useEffect, useState } from "react";
 import useProducts from "../../../hooks/useProducts";
 import { updateProduct } from "../../../api/firebase_db";
 
-const PRODUCT_STATUS = {
-  SALE: "Sale",
-  SOLD_OUT: "Sold Out",
-  HIDE: "Hide",
-};
+const PRODUCT_STATUS_FILTER = ["all", "Sale", "Sold Out", "Hide"];
 export default function ProductStatus() {
   //   const [currnetProductStatus, setCurrentProductStatus] = useState("ALL");
   const productStatus = ["ALL", "Sale", "Sold Out", "Hide"];
@@ -20,43 +16,56 @@ export default function ProductStatus() {
   const test = data ? Object.entries(data[3]) : [];
   const women = data ? Object.entries(data[4]) : [];
 
-  const productList = [accessories, men, shoes, women];
+  const productList = [accessories, men, shoes, women, test];
 
-  const totalLength = data
-    ? productList.reduce((acc, cur) => acc + cur.length, 0)
-    : 0;
+  // const totalLength = data
+  //   ? productList.reduce((acc, cur) => acc + cur.length, 0)
+  //   : 0;
 
-  const saleLength = data
-    ? productList.reduce((acc, productByCategory) => {
-        const filteredProductList = productByCategory.filter((product) => {
-          const productData = product[1];
-          // console.log(productData); //! status를 이제 firebase에 일일이 추가해줘야한다. ㅅㅂ status랑 stock이 없으니까 안나오지 ㅄ
-          return productData.status === PRODUCT_STATUS.SALE;
-        });
-        // console.log(filteredProductList);
-        return acc + filteredProductList;
-      }, 0)
-    : 0;
+  // const saleLength = data
+  //   ? productList.reduce((acc, productByCategory) => {
+  //       const filteredProductList = productByCategory.filter((product) => {
+  //         const productData = product[1];
+  //         return productData.status === PRODUCT_STATUS.SALE;
+  //       });
+  //       return acc + filteredProductList.length;
+  //     }, 0)
+  //   : 0;
 
   // const soldOutLength =
+  function getLength(type) {
+    let length = 0;
+    if (type === "all") {
+      length = data ? productList.reduce((acc, cur) => acc + cur.length, 0) : 0;
+    } else {
+      length = data
+        ? productList.reduce((acc, productByCategory) => {
+            const filteredProductList = productByCategory.filter((product) => {
+              const productData = product[1];
+              return productData.status === type;
+            });
+            return acc + filteredProductList.length;
+          }, 0)
+        : 0;
+    }
+    return length;
+  }
 
   const handleTest = (e) => {
-    for (let i = 0; i < women.length; i++) {
-      const [key, product] = women[i];
-      const updatedProduct = { ...product };
-
-      updatedProduct.stock = 50;
-      updatedProduct.status = "Sale";
-
-      updateProduct(key, updatedProduct);
-    }
+    // for (let i = 0; i < women.length; i++) {
+    //   const [key, product] = women[i];
+    //   const updatedProduct = { ...product };
+    //   updatedProduct.stock = 50;
+    //   updatedProduct.status = "Sale";
+    //   updateProduct(key, updatedProduct);
+    // }
   };
 
   return (
     <ul className="flex gap-4">
       {productStatus.map((item, index) => (
         <li className="p-4 border-b border-black" key={index}>
-          {item} {totalLength}
+          {item} {getLength(PRODUCT_STATUS_FILTER[index])}
         </li>
       ))}
       <div onClick={handleTest}>상품 업데이트</div>
