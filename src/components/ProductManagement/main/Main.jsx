e136973031df8dde03b219a002143b92424e137b
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-01-30 22:26:01 +0900

    Feat : 상품 리스트 메인 완성 및 헤더 텍스트 정렬  #18

--- src/components/ProductManagement/main/Main.jsx
+++ src/components/ProductManagement/main/Main.jsx
@@ -26,38 +26,42 @@ export default function Main() {
         </div>
       </div>
       <div className="mt-4 bg-white">
-        <div className="px-8 py-2 border-b border-gray-300  flex font-bold">
-          <div className="w-16 border border-black">
+        <div className="flex px-8 py-2 border-b border-gray-300 font-bold">
+          <div className="w-20">
             <input type="checkbox" /> No
           </div>
-          <div className="w-96 border border-black">상품명</div>
-          <div className="w-36 border border-black">판매가</div>
-          <div className="w-48 border border-black">카테고리</div>
-          <div className="w-40 border border-black">상태</div>
-          <div className="w-20 border border-black">재고</div>
-          <div className="w-48 border border-black">등록일</div>
+          <div className="w-96 text-center">Title</div>
+          <div className="w-36 text-center">Price</div>
+          <div className="w-48 text-center">Category</div>
+          <div className="w-40 text-center">Status</div>
+          <div className="w-16  text-center mr-4">Stock</div>
+          <div className="w-48 text-center">Registration Date</div>
         </div>
         <div className="">
           <ul>
-            <li className="flex justify-around">
-              <div className="flex">
-                <input type="checkbox" /> <p className="pl-2 text-end">999</p>
+            <li className="flex px-8 py-3">
+              <div className="w-20">
+                <input type="checkbox" />
+                <span className="ml-1">9999</span>
               </div>
-              <div>상품명</div>
-              <div>판매가</div>
-              <div>카테고리</div>
-              <div>상태</div>
-              <div>재고</div>
+              <div className="w-96 text-center">Men's cloth 1</div>
+              <div className="w-36 text-center">1000$</div>
+              <div className="w-48 text-center">Men</div>
+              <div className="w-40 text-center">Sold Out</div>
+              <div className="w-16 text-center mr-4">50</div>
+              <div className="w-48 text-center">2024-01-30</div>
             </li>
-            <li className="flex justify-around">
-              <div className="flex">
-                <input type="checkbox" /> <p className="pl-2 text-end">9</p>
+            <li className="flex px-8 py-3">
+              <div className="w-20">
+                <input type="checkbox" />
+                <span className="ml-1">2</span>
               </div>
-              <div>상품명</div>
-              <div>판매가</div>
-              <div>카테고리</div>
-              <div>상태</div>
-              <div>재고</div>
+              <div className="w-96 text-center">Men's cloth 2</div>
+              <div className="w-36 text-center">500$</div>
+              <div className="w-48 text-center">Men</div>
+              <div className="w-40 text-center">Sale</div>
+              <div className="w-16 text-center mr-4">100</div>
+              <div className="w-48 text-center">2024-01-30</div>
             </li>
             <li></li>
             <li></li>
