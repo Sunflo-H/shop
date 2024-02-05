1717e94aa1577a638840f1a3fc9e7e591b3d992c
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-02-05 23:54:56 +0900

    Feat : 파일명, 변수명 적절히 변경#18

--- src/components/ProductManagement/nav/Nav.jsx
+++ src/components/ProductManagement/nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FiSettings } from "react-icons/fi";
-import Nav_category from "./Nav_category";
+import Category from "./Category";
 
 const categoryList = [["ALL", "Men", "Women", "Shoes", "Accessories"]];
 export default function Nav() {
@@ -15,7 +15,7 @@ export default function Nav() {
       <div className="flex gap-2 text-blue-500 px-8 py-4">
         Management <FiSettings className="self-center" />
       </div>
-      <Nav_category
+      <Category
         currentCategory={currentCategory}
         category={categoryList[0]}
         handleCategoryClick={handleCategoryClick}
