24011a8a91040ae401fb622f12b29e7b1fa4266a
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-02-21 22:35:54 +0900

    Feat : <PageNation> 컴포넌트화 #18
    <PageNumber> 생성

--- src/components/ProductManagement/main/PageNation.jsx
+++ src/components/ProductManagement/main/PageNation.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdKeyboardArrowLeft } from "react-icons/md";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 
 const DATA_LENGTH = 50;
-let page = 1;
+const MAX_VIEW = 5;
+const RANGE = [1, 2, 3, 4, 5];
 let count_showItem = 10;
 
 // 1~5
@@ -14,7 +15,23 @@ let count_showItem = 10;
 // 16~20
 // 21~25
 
+/**
+ * 총 데이터는 50개라고 치자
+ * page를 정해야해
+ * 1,2,3,4,5 count = 0 ; count * RANGE + 1,2,3,4,5
+ * 6,7,8,9,10
+ * ...
+ * 46,47,48,49,50
+ *
+ *
+ *
+ */
+
 export default function PageNation() {
+  const [page, setPage] = useState(0);
+  const handlePageClick = () => {
+    console.log("hi");
+  };
   return (
     <div className="w-full border border-black flex items-center justify-center">
       <div className="flex text-2xl">
@@ -25,14 +42,13 @@ export default function PageNation() {
           <MdKeyboardArrowLeft />
         </div>
       </div>
-      <div className="flex items-center h-10 text-center  font-bold">
-        <div className="px-4 py-1 hover:bg-blue-200 hover:text-blue-500 rounded-md">
-          1
-        </div>
-        <div className="px-4  py-1 hover:bg-blue-200 rounded-md ">2</div>
-        <div className="px-4  py-1 hover:bg-blue-200 rounded-md ">3</div>
-        <div className="px-4  py-1  hover:bg-blue-200 rounded-md">4</div>
-        <div className="px-4  py-1 hover:bg-blue-200 rounded-md ">5</div>
+      <div
+        className="flex items-center h-10 text-center font-bold"
+        onClick={handlePageClick}
+      >
+        {RANGE.map((item, index) => (
+          <PageNumber number={page * 5 + item} />
+        ))}
       </div>
       <div className="flex text-2xl">
         <div className="px-2 py-1 hover:bg-gray-300 rounded-md">
@@ -42,6 +58,14 @@ export default function PageNation() {
           <MdKeyboardDoubleArrowRight />
         </div>
       </div>
+    </div>
+  );
+}
+
+function PageNumber({ number }) {
+  return (
+    <div className="px-4 py-1 hover:bg-blue-200 hover:text-blue-500 rounded-md">
+      {number}
     </div>
   );
 }
