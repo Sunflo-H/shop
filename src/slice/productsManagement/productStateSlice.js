6664661479badc33675378c246a704068e608da8
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-04-27 00:31:11 +0900

    Feat : productStateSlice와 productCategorySlice 생성 후  store 적용 #18

---
+++ src/slice/productsManagement/productStateSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// 외부에서 상품 데이터를 요청 -> slice에 데이터 저장 -> 컴포넌트는 slice로부터 데이터를 가져와서 렌더링한다.
+export const productStateSlice = createSlice({
+  name: "productState",
+  initialState: {
+    statusList: ["ALL", "Sale", "Sold Out", "Hide"],
+  },
+  reducers: {
+    filterByStatus: (state, action) => {
+      state.currentStatus = action.payload;
+
+      if (state.currentStatus === "ALL")
+        state.products = state.products_category;
+      else {
+        state.products = state.products_category.filter(
+          (product) => product[1].status === action.payload
+        );
+      }
+    },
+  },
+});
+
+export const { filterByStatus } = productStateSlice.actions;
+export default productStateSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoryReducer from "./slice/categorySlice";
 import productManagementReducer from "./slice/productsManagement/productManagementSlice";
+import productStateReducer from "./slice/productsManagement/productStateSlice";
+import productCategoryReducer from "./slice/productsManagement/productCategorySlice";
 
 export default configureStore({
   reducer: {
     category: categoryReducer,
     productManagement: productManagementReducer,
+    productState: productStateReducer,
+    productCategory: productCategoryReducer,
   },
 });
